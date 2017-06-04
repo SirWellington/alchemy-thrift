0f80205916f1dee8d0756f13a1232e89bfedf228
Wellington Moreno <jwellington.moreno@gmail.com>
2017-06-03 17:39:36 -0700

    Upgrades test dependencies

--- src/test/java/tech/sirwellington/alchemy/thrift/clients/ClientsTest.java
+++ src/test/java/tech/sirwellington/alchemy/thrift/clients/ClientsTest.java
@@ -24,6 +24,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import tech.sirwellington.alchemy.test.junit.ExceptionOperation;
 import tech.sirwellington.alchemy.test.junit.runners.AlchemyTestRunner;
 import tech.sirwellington.alchemy.test.junit.runners.Repeat;
